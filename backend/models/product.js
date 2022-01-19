01d140281bf234ba8112f27beda3fdadb0f68611
ArkOberon <arcon_ark@protonmail.com>
2022-01-19 10:31:09 -0800

    v0.1

---
+++ backend/models/product.js
@@ -0,0 +1,88 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+
+  name: {
+    type: String,
+    required:[true, 'Please enter product name'],
+    trim: true,
+    maxLength:[100,'Product name cannot exceed 100 characters']
+  },
+  price: {
+    type: Number,
+    required:[true, 'Please enter product name'],    
+    minLength:[3,'Product name cannot exceed 100 characters'],
+    default: 0.00
+  },
+  description: {
+    type: String,
+    required:[true, 'Please enter product description']    
+  },
+  ratings: {
+    type: Number,
+    default: 0   
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true
+      },
+      url: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  category: {
+    type: String,
+    required: [true, 'Please select category for this product'],
+    enum: {
+      values: [
+        'e-Book',
+        'Podcast',
+        'Photos',
+        'Digital Art',
+        'Tutorials',
+        'Images'
+      ],
+      message: 'Please select correct category for this product'
+    }
+  },
+  seller: {
+    type: String,
+    required: [true, 'Pleaseenter product seller']
+  },
+  stock: {
+    type: Number,
+    required: [true, 'Please enter product stock'],
+    minLength: [3, 'Product name cannot exceed 5 characters'],
+    default: 0.00
+  },
+  numofReviews: {
+    type: Number,
+    default: 0
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true
+      },
+      rating: {
+        type: Number,
+        required: true
+      },
+      comment: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }  
+})
+
+module.exports = mongoose.model('Product', productSchema);
