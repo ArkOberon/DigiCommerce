e8e52717b178cf1d9fe7e8625ecc237a57e9bb0d
ArkOberon <arcon_ark@protonmail.com>
2022-01-20 13:09:20 -0800

    v0.1.1

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,8 +8,8 @@ exports.newProduct = async (req, res, next) => {
     res.status(201).json({
         success: true,
         product
-    })
-}
+    });
+};
 
 // Get all products => /api/v1/products
 exports.getProducts = async (req, res, next) => {
@@ -21,8 +21,8 @@ exports.getProducts = async (req, res, next) => {
         count: products.length,
         /*message: 'This route will show all products in database.',*/
         products
-    })
-}
+    });
+};
 
 // Get single product details => /api/v1/product/:id
 exports.getSingleProduct = async (req, res, next) => {
@@ -33,11 +33,54 @@ exports.getSingleProduct = async (req, res, next) => {
         return res.status(404).json({
             success: false,
             message: 'Product not found'
-        })
-    }
+        });
+    };
 
     res.status(200).json({
         success: true,
         product
+    });
+};
+
+// Update Product => /api/v1/product/:id
+exports.updateProduct = async (req, res, next) => {
+
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        });
+    };
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true        
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    });    
+};
+
+// Delete Product => /api/v1/admin/product/:id
+exports.deleteProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        });
+    };
+
+    await product.deleteOne();
+
+    res.status(200).json({
+        success: true,
+        message: 'Product is deleted'
     })
 }
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,13 +4,19 @@ const router = express.Router();
 const { 
   getProducts, 
   newProduct, 
-  getSingleProduct 
+  getSingleProduct,
+  updateProduct,
+  deleteProduct
 } = require('../controllers/productController');
 
+router.route('/product/:id').get(getSingleProduct);   
 router.route('/products').get(getProducts);
-router.route('/product/:id').get(getSingleProduct);
 
-router.route('/product/new').post(newProduct);
+router.route('/admin/product/new').post(newProduct);
+router.route('/admin/product/:id')
+      .get(getSingleProduct)
+      .put(updateProduct)
+      .delete(deleteProduct)
 
 module.exports = router;
 
